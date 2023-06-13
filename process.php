7b456d14a52b75ed5d090be171049f6178e8e162
Anas Baqai <anashabib139@gmail.com>
2023-06-13 21:27:08 +0500

    inital commit

---
+++ process.php
@@ -0,0 +1,43 @@
+<!-- <?php
+
+    // $to = "";
+    // $from = $_REQUEST['name'];
+    // $subject = $_REQUEST['subject'];
+    // $name = $_REQUEST['name'];
+    // $headers = "From: $from";
+
+    // $fields = array();
+    // $fields{"name"} = "name";
+    // $fields{"email"} = "email";
+    // $fields{"subject"} = "subject";
+    // $fields{"message"} = "message";
+
+    // $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+
+    // $send = mail($to, $subject, $body, $headers);
+
+?> -->
+
+<?php
+
+$to = "anasbaqai9@gmail.com";
+$from = $_REQUEST['email'];
+$subject = $_REQUEST['subject'];
+$name = $_REQUEST['name'];
+$headers = "From: $from";
+
+$fields = array();
+$fields["name"] = "name";
+$fields["email"] = "email";
+$fields["subject"] = "subject";
+$fields["message"] = "message";
+
+$body = "Here is what was sent:\n\n";
+foreach ($fields as $a => $b) {
+    $body .= sprintf("%20s: %s\n", $b, $_REQUEST[$a]);
+}
+
+$send = mail($to, $subject, $body, $headers);
+
+?>
+
